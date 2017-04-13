eec9a91c0868ee97729b52aad31308d4b1a33163
yangbo <yangbo@tigerbrokers.com>
2017-04-13 20:38:50 +0800

    Image 组件

--- app/index.js
+++ app/index.js
@@ -6,6 +6,7 @@ import Index from 'pages/Index';
 import Color from 'pages/Color';
 import Icon from 'pages/Icon';
 import Button from 'pages/Button';
+import Image from 'pages/Image';
 import Toast from 'pages/Toast';
 
 const tigerUIElement = document.querySelector('.tiger-ui');
@@ -13,10 +14,11 @@ const tigerUIElement = document.querySelector('.tiger-ui');
 render((
     <Router history={hashHistory}>
         <Route path="/" component={Index}>
-            <IndexRoute component={Color} />
+            <IndexRoute component={Icon} />
             <Route path="/color" component={Color} />
             <Route path="/icon" component={Icon} />
             <Route path="/button" component={Button} />
+            <Route path="/image" component={Image} />
             <Route path="/modal" component={Toast} />
         </Route>
     </Router>
--- app/pages/Index/config.js
+++ app/pages/Index/config.js
@@ -1,8 +1,9 @@
-export const navLists = ['icon', 'button', 'modal', 'color'];
+export const navLists = ['icon', 'button', 'image', 'modal', 'color'];
 
 export const navCn = {
     icon: '图标',
     button: '按钮',
+    image: '图片',
     modal: '模态',
     color: '颜色'
 }
